c109b5e756d315da36918dd6153c5056cafc5e16
Patrick Philips <jppwriter01@gmail.com>
2018-12-17 21:35:28 -0500

    Added user prompt to download.py to enter a SA ID for downloading and echoed to the command line

--- download.py
+++ download.py
@@ -1,3 +1,8 @@
 # download.py takes a Sermon Audio mp3 ID and saves it to the specified
 # directory.
 
+# Prompt the user to enter a Sermon Audio ID for downloading
+sermon_ID = input('Enter a Sermon Audio ID to download: ')
+
+# Display the ID
+print("The Sermon Audio ID was: ", sermon_ID)
